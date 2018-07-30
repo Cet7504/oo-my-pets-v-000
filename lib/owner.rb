5225cb3e0af0ae7650de74b88eafffb08cc2add5
Christian Timberlake II <timberlakece1@my.palmbeachstate.edu>
2018-07-30 04:33:06 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,50 @@
 class Owner
-  # code goes here
+  attr_accessor :name, :pets
+  attr_reader :species
+
+  @@all = []
+
+  def initialize(species)
+    @species = species
+    @pets = {fishes: [], cats: [], dogs: []}
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.reset_all
+    @@all.clear
+  end
+
+  def self.count
+    @@all.size
+  end
+
+  def say_species
+    "I am a #{@species}."
+  end
+
+  def buy_fish(fish_name)
+    @pets[:fishes] << Fish.new(fish_name)
+  end
+
+  def buy_cat(cat_name)
+    @pets[:cats] << Cat.new(cat_name)
+  end
+
+  def buy_dog(dog_name)
+    @pets[:dogs] << Dog.new(dog_name)
+  end
+
+  def walks_dogs
+    @pets[:dogs].each do |dog|
+      dog.mood = "happy"
+    end
+  end
+
+
+
+
 end
